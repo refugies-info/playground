fb09f00ba564487faf9d16f7e3c47bbc572c8981
Luis Arias <luis@refugies.info>
2025-11-24 16:08:14 +0100

    refactor: replace ESLint with Biome for linting and formatting

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -3,7 +3,7 @@
  * Exported for use across frontend and backend workspaces
  */
 
-export type { User } from "./types/user";
-export type { ContentItem } from "./types/content-item";
 export type { ContentFlag } from "./types/content-flag";
+export type { ContentItem } from "./types/content-item";
 export type { MetadataMapping, SupabaseEnv } from "./types/metadata";
+export type { User } from "./types/user";
