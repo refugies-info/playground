cfb19384c917efa8ba5a01e3fda885944154eda3
Camille <cssaillard@gmail.com>
2026-06-30 16:06:20 +0200

    delete unused

--- apps/frontend/src/services/current-user.ts
+++
@@ -1,7 +0,0 @@
-"use server";
-
-import { type CurrentUser, getCurrentUser } from "@/lib/auth";
-
-export async function getCurrentUserAction(): Promise<CurrentUser> {
-  return getCurrentUser();
-}
